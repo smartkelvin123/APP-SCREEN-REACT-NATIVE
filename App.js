8dd3f2d3c1b9cb385678db7c9a43a76829b2e4fe
smartkelvin123 <105611054+smartkelvin123@users.noreply.github.com>
2023-06-29 23:52:53 +0100

    updated code

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import styled from "styled-components/native";
+import Header from "./component/Header";
+import Avatar from "./component/Avatar";
+import InputContainer from "./component/InputContainer";
+import Card from "./component/Card";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: white;
+`;
+
+const SmartContainer = styled.View`
+  width: 100%;
+  padding-horizontal: 10px;
+  flex-direction: row;
+`;
+const DATA = [
+  {
+    id: "1",
+    userAvatar: require("./assets/avatar2.png"),
+    userName: "User 1",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    postImage: require("./assets/post1.png"),
+  },
+  {
+    id: "2",
+    userAvatar: require("./assets/avatar4.png"),
+    userName: "User 2",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    postImage: require("./assets/post2.png"),
+  },
+  {
+    id: "3",
+    userAvatar: require("./assets/avatar3.png"),
+    userName: "User 3",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    postImage: require("./assets/splash.png"),
+  },
+  {
+    id: "4",
+    userAvatar: require("./assets/avatar4.png"),
+    userName: "User 4",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    postImage: require("./assets/post4.png"),
+  },
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Header headerTitle="social" />
+      <SmartContainer>
+        <Avatar imageSource={require("./assets/avatar1.png")} />
+        <InputContainer />
+      </SmartContainer>
+      <Card data={DATA} />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ component/PressableButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const ButtonContainer = styled.TouchableOpacity`
+  margin-vertical: 40px;
+  width: 120px;
+  height: 40px;
+  padding: 12px;
+  border-radius: 10px;
+  background-color: ${(props) => props.bgColor};
+`;
+
+const ButtonText = styled.Text`
+  font-size: 16px;
+  text-align: center;
+  color: ${(props) => (props.primary ? "white" : "#010101")};
+`;
+
+const PressableButton = ({ title, primary, bgColor, OnPress }) => {
+  return (
+    <ButtonContainer OnPress={OnPress} bgColor={bgColor}>
+      <ButtonText primary={primary}>{title}</ButtonText>
+    </ButtonContainer>
+  );
+};
+export default PressableButton;
